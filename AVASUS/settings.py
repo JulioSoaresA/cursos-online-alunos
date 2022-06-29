37e3406775fdd88e74b896dadcc5357f46d4446b
JulioSoaresA <juliocsoaresa1@gmail.com>
2022-06-28 22:47:41 -0300

    Ajustes no index, organizando as pastas, criando partials e menu

--- AVASUS/settings.py
+++ AVASUS/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'AVASUS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lais-backend',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost'
     }
 }
 
